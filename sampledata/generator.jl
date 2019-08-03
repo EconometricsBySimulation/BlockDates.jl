0f6a3bcab27ecbbed142fa4727135c1c915e2c3f
Smart <francis.smart.ctr@ad.dot.gov>
2019-08-03 04:42:33 -0400

    Data Generator two sets

--- sampledata/generator.jl
+++ sampledata/generator.jl
@@ -1,7 +1,8 @@
 cd("C:/Users/francis.smart.ctr/GitDir/SoftDatesJL/SoftDatesJL")
 
-using Pkg, Dates, DataFrames, Distributions
+using Pkg
 Pkg.activate(".")
+using Pkg, Dates, DataFrames, Distributions, CSV
 
 # used lipsum.com to grab 2000 year old document (https://www.lipsum.com/)
 lipsum = read("sampledata/Lipsum.txt", String)
@@ -30,12 +31,11 @@ daydates = [sort(sample(startdt[i]:Day(1):enddt[i], coverage[i], replace = false
 
 notes =  [join(sample(lip_sen, rand(Binomial(3, .25),1)[1]), " ") for i in 1:n]
 
-txtfield = [[join(sample(lip_par, rand([1,1,2,3])), "\n\r") for j in 1:length(daydates[i])]  for i in 1:n]
+txtfield = [[join(sample(lip_sen, rand([1,1,2,3])), "\n\r") for j in 1:length(daydates[i])]  for i in 1:n]
 
+sep = sample(split(raw"./- ","") , n)
 
-sep = sample(split(raw".\/- ","") , n)
-
-txtblocks = [join((notes[i], , ), )]
+# txtblocks = [join((notes[i], , ), )]
 
 # We have start dates and end dates and where things get tricky is that we
 # have individual records that start at Start and end at End. They are listed
@@ -50,16 +50,141 @@ V1 = [join(sample(lip_wrd, rand(Binomial(9, .25),1)[1]), " ") for i in 1:n]
 V2 = [sample(lip_sen, 1)[1] for i in 1:n]
 V3 = [sample(1:(n*5), 1)[1] for i in 1:n]
 
-txtblocks =
-   [ begin
-   x = [join([Dates.format.(daydates[i][j],"yyyy$(sep[i])mm$(sep[i])dd"), txtfield[i][j]]," ") for j in 1:length(daydates[i])]
+# Generate the text fields
+[ begin
+   x = [join([Dates.format.(daydates[i][j],"dd$(sep[i])mm$(sep[i])yyyy"), txtfield[i][j]]," ") for j in 1:length(daydates[i])]
    replace(join([notes[i], x...], "\n\r"), r"^\n\r" => "")
    end
    for i = 1 : n ]
 
-dt1 = DataFrame(Start = startdt ,
-            End = enddt,
-            Dates = daydates,
-            Text = txtblocks,
-            V0=V0, V1=V1, V2=V2, V3=V3,
-            type = 1)
+# Generate date type 1
+fmlist = [
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yy"   for i in 1:n] ,
+  ["yyyy$(sep[i])mm$(sep[i])dd" for i in 1:n] ,
+  ["yy$(sep[i])mm$(sep[i])dd"   for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yyyy" for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yy"   for i in 1:n] ]
+
+txtblocks =
+    [begin
+     [ begin
+     x = [join([Dates.format.(daydates[i][j], fmlist[ii][i]), txtfield[i][j]]," ")
+       for j = 1:length(daydates[i])]
+     replace(join([notes[i], x...], "\n\r"), r"^\n\r" => "")
+     end
+     for i = 1 : n ]
+    end
+    for ii = 1:length(fmlist)]
+
+dt = DataFrame(
+  type = 1,
+  Start = startdt ,
+  End = enddt,
+  Dates = daydates,
+  Text = txtblocks[1],
+  V0=V0, V1=V1, V2=V2, V3=V3)
+
+for i in 2:length(fmlist)
+    append!(dt,
+     DataFrame(type = i,
+      Start = startdt ,
+      End = enddt,
+      Dates = daydates,
+      Text = txtblocks[i],
+      V0=V0, V1=V1, V2=V2, V3=V3))
+    end
+
+CSV.write("sampledata/SampleDates.csv", dt)
+
+####################################
+
+dayranges =
+  [begin
+    y = daydates[i]
+    x = sort(sample(1:length(y), minimum([2,length(y)]), replace = false))
+    (length(x) == 2) ? ([y[1:(x[1]-1)], [y[x[1]],y[x[2]]], y[(x[2]+1):end]]) : [y, y[1:0], y[1:0]]
+   end
+   for i in 1:n]
+
+seprange = sample( ["-", "thru", "through", "till"] , n)
+
+# Generate date type 1
+fm2list = [
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yy"   for i in 1:n] ,
+  ["yyyy$(sep[i])mm$(sep[i])dd" for i in 1:n] ,
+  ["yy$(sep[i])mm$(sep[i])dd"   for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yyyy" for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yy"   for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yy" for i in 1:n] ]
+
+fr1list = [
+    ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+    ["dd$(sep[i])mm$(sep[i])yy"   for i in 1:n] ,
+    ["yyyy$(sep[i])mm$(sep[i])dd" for i in 1:n] ,
+    ["yy$(sep[i])mm$(sep[i])dd"   for i in 1:n] ,
+    ["mm$(sep[i])dd$(sep[i])yyyy" for i in 1:n] ,
+    ["mm$(sep[i])dd$(sep[i])yy"   for i in 1:n] ,
+    ["mm$(sep[i])dd" for i in 1:n] ,
+    ["mm$(sep[i])dd" for i in 1:n]]
+
+fr2list = [
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yy"   for i in 1:n] ,
+  ["yyyy$(sep[i])mm$(sep[i])dd" for i in 1:n] ,
+  ["yy$(sep[i])mm$(sep[i])dd"   for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yyyy" for i in 1:n] ,
+  ["mm$(sep[i])dd$(sep[i])yy"   for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ,
+  ["dd$(sep[i])mm$(sep[i])yyyy" for i in 1:n] ]
+
+
+dtrange = dt[1:0,]
+
+for k in 1:length(fm2list)
+
+  textblock = [ begin
+     dtr = dayranges[i]
+      x1 =
+        if length(dtr[1]) > 0
+        join([join([Dates.format(dtr[1][j],fm2list[k][i]), txtfield[i][j]]," ")
+              for j in 1:length(dtr[1])], "\n\r")
+        else ""
+        end
+
+      x2 = if length(dtr[2]) == 2
+        d1 = Dates.format(dtr[2][1], fr1list[k][i])
+        sepr = seprange[i]
+        d2 = Dates.format(dtr[2][2], fr2list[k][i])
+        t1 = txtfield[i][length(dtr[1])+1]
+        "$d1 $sepr $d2 $t1"
+        else ""
+        end
+
+      x3 =
+       if length(dtr[3]) > 0
+         join([join([Dates.format(dtr[3][j],fm2list[k][i]), txtfield[i][j]]," ")
+           for j in 1:length(dtr[3])], "\n\r")
+       else ""
+       end
+
+      replace(join([x1, x2, x3], "\n\r"), r"^\n\r"=>"")
+     end
+     for i = 1 : n ]
+
+    df_temp = DataFrame(
+        type = k,
+        Start = startdt ,
+        End = enddt,
+        Dates = daydates,
+        Text = textblock,
+        V0=V0, V1=V1, V2=V2, V3=V3)
+
+    append!(dtrange, df_temp)
+end
+
+dtrange
+
+CSV.write("sampledata/SampleDateRanges.csv", dtrange)
